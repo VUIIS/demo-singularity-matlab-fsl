6e80c19e446bfc41e1987211d408ea747cef5238
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-03 13:26:08 -0500

    test-compiled

---
+++ matlab/build/test_compiled_matlab.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# This script allows testing the compiled matlab, assuming the correct matlab
+# runtime is installed. Much better to make sure it's working before actually 
+# building the singularity container.
+
+../bin/run_matlab_entrypoint.sh /usr/local/MATLAB/MATLAB_Runtime/v97 \
+    image_niigz ../../INPUTS/t1.nii.gz \
+    diameter_mm 30 \
+    out_dir ../../OUTPUTS
