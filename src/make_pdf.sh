fe3f3b8ba501e83d7a0ade83afbedc9e3d3ef3a3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-03 16:59:06 -0500

    pdf

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -18,13 +18,13 @@ cd ${out_dir}
 
 # Binarize the segmentation and find its center of mass, as a starting point for
 # slice placement in the visualization
-fslmaths seg_niigz -bin mask
+fslmaths "${seg_niigz}" -bin mask
 com=$(fslstats mask -c)
 XYZ=(${com// / })
 
 # Create an axial view of T1 with ROI overlay for a series of axial slices. 
 # Offsets are specified in mm from the computed center of mass of the brain.
-for sl in -40 -30 -20 -10 +0 +10 +20 +30 +40 +50 +60; do
+for sl in -040 -030 -020 -010 000 010 020 030 040 050 060; do
 
     Z=$(echo "${XYZ[2]} + ${sl}" | bc -l)
     fsleyes render -of slice_${sl}.png \
@@ -35,21 +35,22 @@ for sl in -40 -30 -20 -10 +0 +10 +20 +30 +40 +50 +60; do
 
 done
 
-# Combine into single image using ImageMagick
-montage -mode concatenate slice_*.png \
--tile 4x3 -quality 100 -background black -gravity center \
--border 20 -bordercolor black page1.png
+# Combine into single image using ImageMagick. Pretty hacky way of getting the
+# slices in the right order, but it works
+montage -mode concatenate slice_-0{4,3,2,1}*.png slice_0*.png \
+    -tile 3x4 -quality 100 -background black -gravity center \
+    -border 20 -bordercolor black page1.png
 
 # Resize and add text annotations. We choose a large but not ridiculous
 # pixel size for the full page.
 convert \
--size 2600x3365 xc:white \
--gravity center \( page1.png -resize 2400x \) -composite \
--gravity North -pointsize 48 -annotate +0+100 \
-"Holed segmentation on holed T1" \
--gravity SouthEast -pointsize 48 -annotate +100+100 "${thedate}" \
--gravity NorthWest -pointsize 48 -annotate +100+200 "${infostring}" \
-page1.png
+    -size 2600x3365 xc:white \
+    -gravity center \( page1.png -resize 2400x \) -composite \
+    -gravity North -pointsize 48 -annotate +0+100 \
+    "Holed segmentation on holed T1" \
+    -gravity SouthEast -pointsize 48 -annotate +100+100 "${thedate}" \
+    -gravity NorthWest -pointsize 48 -annotate +100+200 "${infostring}" \
+    page1.png
 
 # Convert to PDF
 convert page1.png holed_image.pdf
