db35ceab9506ce0e1150f1ac4243a9c16d8cf032
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-07 10:54:36 -0500

    logging

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -23,6 +23,8 @@ XYZ=(${com// / })
 for sl in -040 -030 -020 -010 000 010 020 030 040 050 060; do
 
     Z=$(echo "${XYZ[2]} + ${sl}" | bc -l)
+    echo "    Slice ${sl} at ${XYZ[0]} ${XYZ[1]} ${Z}"
+
     fsleyes render -of slice_${sl}.png \
         --scene ortho --worldLoc ${XYZ[0]} ${XYZ[1]} ${Z} \
         --layout horizontal --hideCursor --hideLabels --hidex --hidey \
